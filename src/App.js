0914c0517c36bcbff87fe68d2015d8391cd2a585
Adson Wesley <adsonxzx@gmail.com>
2019-10-08 21:48:14 -0300

    finalizando tela de login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from 'react'
+import './App.css'
+
+import logo from './assets/logo.svg'
 
 function App() {
   return (
-    <h1>hello worlds</h1>
+    <div className="container">
+      <img src={logo} alt=""/>
+
+      {/* Form */}
+      <div className="form-box">
+        <p className="text">Ofereça <b>spots</b> para programadores e
+          encontre <b>talentos</b> para sua enpresa
+        </p>
+
+        <form>
+          <label htmlFor="email"> E-MAIL * </label>
+          <input 
+            type="text" 
+            id="email" 
+            name="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn-cadastrar">Entrar</button>
+        </form>
+      </div>
+    </div>
+    
   );
 }
 
